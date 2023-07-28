819f7e51995f88b11b74b3ff94ea28518f090f77
Arnat Ngawsuwan <annsw.work@gmail.com>
2023-07-28 18:53:49 +0700

    fix social icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      '2xl': {'min': '1535px'},
+      // => @media (min-width: 1535px) { ... }
+
+      'xl': {'min': '1279px'},
+      // => @media (min-width: 1279px) { ... }
+
+      'lg': {'min': '1025px'},
+      // => @media (min-width: 1023px) { ... }
+
+      'md': {'min': '767px'},
+      // => @media (min-width: 767px) { ... }
+
+      'sm': {'min': '639px'},
+      // => @media (min-width: 639px) { ... }
+    }
   },
   plugins: [],
 }
